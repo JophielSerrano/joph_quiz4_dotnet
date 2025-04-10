1de1d4fa8dab974317eabf353778ab3108215895
JophielSerrano <jserra27@my.centennialcollege.ca>
2025-04-10 14:12:59 -0400

    Add project files.

---
+++ joph_quiz4_dotnet/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using joph_quiz4_dotnet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace joph_quiz4_dotnet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.DatabaseMessage =
+            "Database access has already been done on April 1, 2025 by Joph";
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
